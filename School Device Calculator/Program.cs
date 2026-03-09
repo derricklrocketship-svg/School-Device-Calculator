fc0dcd423ba8675e6fd3e9ff6de231bc1ac8890b
derri <derri>
2026-03-10 10:31:20 +1300

    Add project files.

---
+++ School Device Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace School_Device_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The starter words for my school device calculator
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                 █████████           █████                        ████                        \r\n                ███▒▒▒▒▒███         ▒▒███                        ▒▒███                        \r\n               ▒███    ▒▒▒   ██████  ▒███████    ██████   ██████  ▒███                        \r\n               ▒▒█████████  ███▒▒███ ▒███▒▒███  ███▒▒███ ███▒▒███ ▒███                        \r\n                ▒▒▒▒▒▒▒▒███▒███ ▒▒▒  ▒███ ▒███ ▒███ ▒███▒███ ▒███ ▒███                        \r\n                ███    ▒███▒███  ███ ▒███ ▒███ ▒███ ▒███▒███ ▒███ ▒███                        \r\n               ▒▒█████████ ▒▒██████  ████ █████▒▒██████ ▒▒██████  █████                       \r\n                ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒  ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒   ▒▒▒▒▒▒  ▒▒▒▒▒                        \r\n                                                                                              \r\n                                                                                              \r\n                                                                                              \r\n                ██████████                         ███                                        \r\n               ▒▒███▒▒▒▒███                       ▒▒▒                                         \r\n                ▒███   ▒▒███  ██████  █████ █████ ████   ██████   ██████                      \r\n                ▒███    ▒███ ███▒▒███▒▒███ ▒▒███ ▒▒███  ███▒▒███ ███▒▒███                     \r\n                ▒███    ▒███▒███████  ▒███  ▒███  ▒███ ▒███ ▒▒▒ ▒███████                      \r\n                ▒███    ███ ▒███▒▒▒   ▒▒███ ███   ▒███ ▒███  ███▒███▒▒▒                       \r\n                ██████████  ▒▒██████   ▒▒█████    █████▒▒██████ ▒▒██████                      \r\n               ▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒     ▒▒▒▒▒    ▒▒▒▒▒  ▒▒▒▒▒▒   ▒▒▒▒▒▒                       \r\n                                                                                              \r\n                                                                                              \r\n                                                                                              \r\n   █████████            ████                      ████             █████                      \r\n  ███▒▒▒▒▒███          ▒▒███                     ▒▒███            ▒▒███                       \r\n ███     ▒▒▒   ██████   ▒███   ██████  █████ ████ ▒███   ██████   ███████    ██████  ████████ \r\n▒███          ▒▒▒▒▒███  ▒███  ███▒▒███▒▒███ ▒███  ▒███  ▒▒▒▒▒███ ▒▒▒███▒    ███▒▒███▒▒███▒▒███\r\n▒███           ███████  ▒███ ▒███ ▒▒▒  ▒███ ▒███  ▒███   ███████   ▒███    ▒███ ▒███ ▒███ ▒▒▒ \r\n▒▒███     ███ ███▒▒███  ▒███ ▒███  ███ ▒███ ▒███  ▒███  ███▒▒███   ▒███ ███▒███ ▒███ ▒███     \r\n ▒▒█████████ ▒▒████████ █████▒▒██████  ▒▒████████ █████▒▒████████  ▒▒█████ ▒▒██████  █████    \r\n  ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒▒▒    ▒▒▒▒▒   ▒▒▒▒▒▒  ▒▒▒▒▒     ");
+            Console.ForegroundColor= ConsoleColor.White;
+            
+            Console.WriteLine("\n\nWelcome to the School Device Calculator.\n\rThis program helps schools calculate the insurance value of their smart devices.\n\rYou will be asked to enter the device name, number of devices, cost per device, and the device category.\n\rThe program will apply the insurance discount rules, calculate the total insured value, and show how the\n\rdevice value decreases by 5% each month over six months.");
+
+            Console.WriteLine(OneDivice);
+
+
+
+
+
+        }
+
+        public static string OneDivice()
+        {
+
+            
+
+
+            return "hello world";
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
